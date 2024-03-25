e7fbbadcbb5a210b324d034321f59cbc6bbd853e
Barbara Barbabela <barbarabarbabela@gmail.com>
2024-03-25 19:31:57 -0300

    iniciando um projeto backend crud

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 
 const app = express();
 const port = 3000;
